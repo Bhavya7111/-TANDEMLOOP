d5b886d8387c8ca775fa7b5e4977314f5fd34bb3
Bhavya Karnam <108254330+Bhavya7111@users.noreply.github.com>
2025-04-10 10:21:14 +0530

    Create Program-4.java

---
+++ Program-4.java
@@ -0,0 +1,21 @@
+package Tandem;
+import java.util.Scanner;
+public class SeriesOFNumber
+{
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a number (a): ");
+            int a = scanner.nextInt();
+            for (int i = 1; i <= a; i++) {
+                int num = 1;
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(num + " ");
+                    num = num + 2;
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+
