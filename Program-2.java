4b4c1923215895c50f6237c94faaa1bf7a603a85
Bhavya Karnam <108254330+Bhavya7111@users.noreply.github.com>
2025-04-10 10:22:42 +0530

    Update Program-2.java

--- Program-2.java
+++ Program-2.java
@@ -1,23 +1,20 @@
 package Tandem;
-import java.util.Scanner;
-public class SeriesOFNumber
-{
-    public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a number (a): ");
-            int a = scanner.nextInt();
-            for (int i = 1; i <= a; i++) {
-                int num = 1;
-                for (int j = 1; j <= i; j++) {
-                    System.out.print(num + " ");
-                    num = num + 2;
+import java.util.*;
+public class CountOfNumber {
+          public static void main(String[] args) {
+            int[] input = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+            Map<Integer, Integer> result = new HashMap<>();
+            for (int i = 1; i <= 9; i++) {
+                int count = 0;
+                for (int num : input) {
+                    if (num % i == 0) {
+                        count++;
+                    }
                 }
-
-                // Move to the next line after each row
-                System.out.println();
+                result.put(i, count);
             }
+            System.out.println(result);
         }
     }
-
 
 
